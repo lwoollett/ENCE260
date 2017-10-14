8389a4e8f981bd0bbaf5807c4e37b668e8dfe0e6
James Patrick Coleman <jco136@cs14180jm.canterbury.ac.nz>
2017-10-14 20:30:32 +1300

    Added starting screen

--- game.c
+++ game.c
@@ -17,6 +17,36 @@ void display_char(char c) //Small function to display a character on the screen
     tinygl_text(to_display);
 }
 
+void display_message (char* message)
+{
+    
+    uint8_t counter = 0;
+    
+    tinygl_init(P_TIME);
+    tinygl_font_set(&font5x7_1);
+    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+
+
+    tinygl_text(message);
+
+    pacer_init (P_TIME);
+    
+    while(counter == 0)
+    {
+        pacer_wait();
+        
+
+        navswitch_update ();
+        tinygl_update();
+        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+            counter++;
+        }
+    }
+    
+}
+
+
 int checkwin(char me, char you){ //Function for checking if a player wins.
     if (me == you) { //Draw scenario
       return 2;
@@ -37,7 +67,10 @@ int main (void)
 {
     char PSR[3] = {'P', 'S', 'R'}; //The char array that holds our options.
     int chosen = 0; //Our counter for figuring out where in the array we are.
-    int counter = 0;
+    
+    uint8_t start_counter = 0;
+    uint8_t end_counter = 0;
+    
     //More setup functions.
     system_init ();
     tinygl_init (P_TIME);
@@ -53,6 +86,7 @@ int main (void)
     //There has been an error with the receiving of the data.
 
     ir_uart_init();
+    
 
     pacer_init (P_TIME);
 
@@ -61,6 +95,16 @@ int main (void)
         pacer_wait ();
         tinygl_update ();
         navswitch_update ();
+
+
+        if (start_counter == 0) {
+            display_message("READY?");
+        }
+        if (start_counter < 100) {
+            start_counter++;
+        }
+
+
 
         if (recstatus == 0){ //While you havent recieved a packet
             if (ir_uart_read_ready_p() != 0) { //If its ready to recieve a char
@@ -73,7 +117,7 @@ int main (void)
             }
         }
 
-        if (sentstatus == 0) //While you havent sent a packet
+        if (sentstatus == 0 && start_counter == 100) //While you havent sent a packet
         {
             if (navswitch_push_event_p (NAVSWITCH_WEST)) { //If the stick has been flicked to the left
                 chosen -= 1;
@@ -105,14 +149,15 @@ int main (void)
             } else if (winstatus == 2) { //Drawn games suck
               display_char('D');
             }
-            counter += 1;
+            end_counter += 1;
             //Display char stuff is temp code.
-            if (navswitch_push_event_p (NAVSWITCH_PUSH) && counter > 100) { //Push in the navswitch
+            if (navswitch_push_event_p (NAVSWITCH_PUSH) && end_counter > 100) { //Push in the navswitch
                 sentstatus = 0; //To start a new game
                 recstatus = 0; //So reset these vars
-                counter = 0;
+                start_counter = 0;
                 chosen = 0;
                 recchar = 'x';
+                end_counter = 0;
             }
 
         } else {
